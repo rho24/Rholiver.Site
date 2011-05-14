3b20273c93864a0db2bb2ed9f3e2ca6d35505030
Richard Oliver <richard@rholiver.co.uk>
2011-05-14 20:02:24 +0100

    Added blog

---
+++ Rholiver.Site/Controllers/BlogController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Rholiver.Site.Models;
+
+namespace Rholiver.Site.Controllers
+{
+    public class BlogController : Controller
+    {
+        //
+        // GET: /Blog/
+
+        public ActionResult Index()
+        {
+            var blogs = new List<BlogPost>()
+            {
+                new BlogPost(){
+                    Id = "first-post",
+                    Title = "First post",
+                    Content = new HtmlString("<p>This is my first post!</p>"),
+                    Created = new DateTime(2011, 05, 14, 19, 42, 00)
+                },
+                new BlogPost(){
+                    Id = "second-post",
+                    Title = "Second post",
+                    Content = new HtmlString("<p>This is my second post.</p><p>Lets try <b>styling</b>.</p>"),
+                    Created = new DateTime(2011, 05, 14, 20, 01, 00)
+                }
+            };
+
+            return View(blogs.OrderByDescending(b => b.Created));
+        }
+
+    }
+}
---
+++ Rholiver.Site/Models/BlogPost.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Rholiver.Site.Models
+{
+    public class BlogPost
+    {
+        [Required]
+        public string Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public HtmlString Content { get; set; }
+
+        [Required]
+        public DateTime Created { get; set; }
+    }
+}
